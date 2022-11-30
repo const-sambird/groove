38a91781b70edcef14bc82743416e6582322a7c4
allineibraimo <89539309+allineibraimo@users.noreply.github.com>
2022-11-30 01:23:02 -0600

    made final register&login changes

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const session = require('express-session');
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
-
-dotenv.config({ path: './.env'});
+const bcrypt = require('bcryptjs');
 
 const { host, user, password, database } = require('./credentials.json').DATABASE;
 const index = require('./routes/index');
 const users = require('./routes/users');
+const { builtinModules } = require('module');
 
 const app = express();
 
@@ -24,16 +22,18 @@ app.get('/', function(request, response) {
 // we're going to need this later but i'll disable it for now
 
 const connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    email: process.env.DATABASE_EMAIL,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE 
+    host: 'localhost',
+	database: 'database',
+    user: 'root',
+    email: 'test',
+    password: ''
 });
 
 connection.connect(err => {
     if (err) console.log(err);
 });
+
+module.exports = connection;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -85,48 +85,65 @@ app.post('/register', function(request, response) {
 	// Ensure the input fields exists and are not empty
 	if (user && password && email) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ? AND email = ?', [user, email, password], function(error, results, fields) {
+		connection.query('SELECT username FROM accounts WHERE username = ?', [user], async(error, results) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
-				/*// Authenticate the user
-				request.session.loggedin = true;
-				request.session.user = user;
-				// Redirect to home page
-				response.redirect('/Location.html');
-				*/
 				return response.render('register', {
 					message: 'The email is already registered. Try logging in!'
 				})
-			} else {
+			} 
+			else {
+
 				response.send('Successfully registered!');
 			}			
-			response.end();
+			
+			let hashedPassword = await bcrypt.hash(password, 8);
+
+			connection.query('INSERT INTO accounts SET ?', {user: user, email: email, password: hashedPassword}, (error, results) => {
+				if (error) {
+					console.log(error);
+				}
+				else {
+					return response.render('register', {
+						message: 'User registered'
+					})
+				}
+			})
+
 		});
 	} else {
 		response.send('Please enter Spotify Username, Email and Password!');
 		response.end();
 	}
-  });
+});
 
 
 app.post('/login', function(request, response) {
 	// Capture the input fields
-	let username = request.body.username;
+	let user = request.body.username;
 	let password = request.body.password;
 	// Ensure the input fields exists and are not empty
-	if (username && password) {
+	if (user && password) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT * FROM accounts WHERE user = "${user}" ', function(error, data) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
+			if (data.length > 0) 
+			{
+				for(var count = 0; count < data.length; count++)
+				{
+					if(data[count].password == password)
+					{
+						request.session.user = data[count].user;
+					}
+					else
+					{
+						response.send('Incorrect password');
+					}
+				}
 				response.redirect('/home');
 			} else {
 				response.send('Incorrect Username and/or Password!');
