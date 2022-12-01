58c07e58892c52984c90b3dd316471c72eb8527b
allineibraimo <89539309+allineibraimo@users.noreply.github.com>
2022-12-01 00:58:40 -0600

    still making changes, need to modify routers

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,94 @@ const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 const router = express.Router();
 
+
+var database = require('../app');
+
+router.get('/', function(req, res, next) {
+    res.render('index', )
+})
+
+router.post('/register', function(request, response) {
+	// Capture the input fields
+	let user = request.body.username-reg;
+	let password = request.body.pwd-reg;
+    let email = request.body.email-reg;
+	// Ensure the input fields exists and are not empty
+	if (user && password && email) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		database.query('SELECT username FROM accounts WHERE username = ?', [user], async(error, results) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				return response.render('register', {
+					message: 'The email is already registered. Try logging in!'
+				})
+			} 
+			else {
+
+				response.send('Successfully registered!');
+			}			
+			
+			let hashedPassword = await bcrypt.hash(password, 8);
+
+			connection.query('INSERT INTO accounts SET ?', {user: user, email: email, password: hashedPassword}, (error, results) => {
+				if (error) {
+					console.log(error);
+				}
+				else {
+					return response.render('register', {
+						message: 'User registered'
+					})
+				}
+			})
+
+		});
+	} else {
+		response.send('Please enter Spotify Username, Email and Password!');
+		response.end();
+	}
+});
+
+
+router.post('/login', function(request, response) {
+	// Capture the input fields
+	let user = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (user && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE user = "${user}" ', function(error, data) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (data.length > 0) 
+			{
+				for(var count = 0; count < data.length; count++)
+				{
+					if(data[count].password == password)
+					{
+						request.session.user = data[count].user;
+					}
+					else
+					{
+						response.send('Incorrect password');
+					}
+				}
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+
 const { client_id, client_secret, scopes } = require('../credentials.json').SPOTIFY;
 
 /* GET home page. */
