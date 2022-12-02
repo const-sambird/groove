5f9ab6f6b77bb650bdd4ea2247ed1a397c737951
Sam Bird <sam@sambird.dev>
2022-12-02 17:34:27 -0600

    rm email and mentions of 'spotify' username

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,10 @@ router.post('/register', function(request, response) {
     
     // Capture the input fields
 	var user = request.body.user;
-    var email_add = request.body.email_add;
 	var password = request.body.password;
    
 
-    database.query('SELECT email FROM accounts WHERE email = "${email_add}" ', (error, results) => {
+    database.query('SELECT user FROM accounts WHERE user = "${user}" ', (error, results) => {
         if(error)
         {
            throw error;
@@ -20,13 +19,13 @@ router.post('/register', function(request, response) {
         if(results.length > 0)
         {
             return response.render('/register', {
-                message: 'The email is already in use. Simply log in!'
+                message: 'The username is already in use. Simply log in!'
             });
         }
 
-        database.query('INSERT INTO accounts SET ?', {user: user, email: email_add, password: password}, (error, results) => {
+        database.query('INSERT INTO accounts SET ?', {user: user, password: password}, (error, results) => {
             console.log(results);
-            return response.render('lLocation');
+            return response.render('location');
         });
     })
 });
