5d1c0ee37c45100f7c20dbf453c08c3f24089e00
allineibraimo <89539309+allineibraimo@users.noreply.github.com>
2022-12-02 22:01:26 -0600

    create concerts.html

--- app.js
+++ app.js
@@ -3,30 +3,15 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-//const mysql = require('mysql');
 const session = require('express-session');
 
 const { host, user, password, database } = require('./credentials.json').DATABASE;
 const pages = require('./routes/pages');
-const users = require('./routes/users');
 const auth = require('./routes/auth');
+//const users = require('./routes/users');
+
 
 const app = express();
-
-// we're going to need this later but i'll disable it for now
-/*
-const connection = mysql.createConnection({
-    host: 'localhost',
-	database: 'database',
-    user: 'root',
-    email: 'test',
-    password: ''
-});
-
-connection.connect( (err) => {
-    if (err) console.log(err);
-});
-*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -50,7 +35,7 @@ app.use(session({
 app.use('/', require('./routes/index'))
 app.use('/', pages);
 app.use('/auth', auth);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
